384373c6c1c101a80a67d18b0cb320940bf18784
kallelzied <kallelzied@live.com>
2018-03-31 14:54:52 +0200

    create UserDTO

---
+++ src/main/java/com/code/street/dto/UserDTO.java
@@ -0,0 +1,103 @@
+package com.code.street.dto;
+
+import com.code.street.validation.PasswordMatches;
+import com.code.street.validation.ValidEmail;
+import com.code.street.validation.ValidPassword;
+
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@PasswordMatches
+public class UserDTO {
+    @NotNull
+    @Size(min = 1, message = "{Size.userDto.firstName}")
+    private String firstName;
+
+    @NotNull
+    @Size(min = 1, message = "{Size.userDto.lastName}")
+    private String lastName;
+
+    @ValidPassword
+    private String password;
+
+    @NotNull
+    @Size(min = 1)
+    private String matchingPassword;
+
+    @ValidEmail
+    @NotNull
+    @Size(min = 1, message = "{Size.userDto.email}")
+    private String email;
+
+    @AssertTrue(message = "You have to accept terms and contracts")
+    private boolean acceptTerms;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    private Integer role;
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(final Integer role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public String getMatchingPassword() {
+        return matchingPassword;
+    }
+
+    public void setMatchingPassword(final String matchingPassword) {
+        this.matchingPassword = matchingPassword;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("UserDto [firstName=")
+                .append(firstName).append(", lastName=")
+                .append(lastName)
+                .append(", password=")
+                .append(password)
+                .append(", matchingPassword=")
+                .append(matchingPassword)
+                .append(", email=")
+                .append(email)
+                .append(", role=")
+                .append(role).append("]");
+        return builder.toString();
+    }
+
+}
