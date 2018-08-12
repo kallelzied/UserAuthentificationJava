d2f93ee18b6151cd24fa828553b5dd7cd44e98ff
kallelzied <kallelzied@live.com>
2018-08-12 23:05:22 +0200

    Added DAO

---
+++ src/main/java/com/code/street/DAO/UserDao.java
@@ -0,0 +1,13 @@
+package com.code.street.DAO;
+
+import com.code.street.dto.UserDTO;
+import com.code.street.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+
+    public User saveUser(UserDTO userDTO) {
+        return null;
+    }
+}
